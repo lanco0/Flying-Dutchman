38364c0da6da53e3c3ea4af5a095528debb04292
lanco0 <77984587+lanco0@users.noreply.github.com>
2023-07-15 20:58:48 +0300

    update

---
+++ Flying-Dutchman/Assets/Scrippt/Healt.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CanBari : MonoBehaviour
+{
+    public float can;
+    public float animasyonYavasligi;
+    private float maxCan;
+    private float gercekScale;
+
+    void Start()
+    {
+        maxCan = can;
+    }
+
+    void Update()
+    {
+        gercekScale = can / maxCan;
+
+        if (transform.localScale.x > gercekScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x - (transform.localScale.x - gercekScale) / animasyonYavasligi, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && can>0 )
+        {
+            can -= 10;
+        }
+        if (can < 0) ;
+        {
+            can = 0;
+        }
+    }
+}
+
+
